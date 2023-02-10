3ad40bfa655881a435e50ba8dff005a81db9c9c0
alex perez <63089422+alexperez21@users.noreply.github.com>
2023-02-10 13:57:03 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+  static Bank BankofDhruva = new Bank();
+  public static void main(String[] args) throws IOException {
+    BankofDhruva.getLogin();
+  }
+}
